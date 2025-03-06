8ac3d9d6e9e4086d3d68e56eb2daf3a0614bab57
nicklm0 <nicklemoncito98@gmail.com>
2025-03-06 13:01:39 +0800

    create some changes to the resolvers and api listings

--- src/ datasources/listing-api.ts
+++ src/ datasources/listing-api.ts
@@ -3,13 +3,22 @@ grab the datalisting on the souce using
 this api code
 forgot to install the npm install @apollo/datasource-rest
 */
-import { RESTDataSource } from "@apollo/datasource-rest";
-import {Listing } from "../types" //import the listing from the types
+ 
 
+//put the import below to the  ListingAPI file
+import { Listing } from "../types";
+import { RESTDataSource } from "@apollo/datasource-rest";
+import { Listing } from "../types";
 
 export class ListingAPI extends RESTDataSource {
-    baseURL =  "https://rt-airlock-services-listing.herokuapp.com/";
-    getFeaturedListings(){
-        return this.get<any[]>("featured-listings");
-    }
+  
+  baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
+  
+  getFeaturedListings(): Promise<Listing[]> {
+    return this.get<Listing[]>("featured-listings");
+  }
+  
+  getListing(listingId: string): Promise<Listing> {
+    return this.get<Listing>(`listings/${listingId}`);
+  }
 }
--- src/resolver.ts
+++ src/resolver.ts
@@ -1,6 +1,15 @@
 //create the resolver code 
-Query: {
-    featuredListings: (_, __, { dataSources }) => {
+
+//create some changes to the resolvers
+import { Resolvers } from './types'
+
+export const resolvers: Resolvers = {
+  Query: {
+    featuredListings: (_, __, {dataSources}) => {
       return dataSources.listingAPI.getFeaturedListings();
     },
-  }
+    listing: (_, { id }, { dataSources }) => {
+      return dataSources.listingAPI.getListing(id);
+    },
+  },
+};
