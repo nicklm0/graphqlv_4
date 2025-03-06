08fe25e3b86eb72753ffe4d853fad8616bc06338
nicklm0 <nicklemoncito98@gmail.com>
2025-03-06 13:15:44 +0800

    chore update the files

--- src/ datasources/listing-api.ts
+++ src/ datasources/listing-api.ts
@@ -6,17 +6,29 @@ forgot to install the npm install @apollo/datasource-rest
  
 //put the import below to the  ListingAPI file
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { Listing } from "../types";
+import { Listing, Amenity, CreateListingInput } from "../types";
 
 export class ListingAPI extends RESTDataSource {
-  
   baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
-  
+
   getFeaturedListings(): Promise<Listing[]> {
     return this.get<Listing[]>("featured-listings");
   }
-  
+
   getListing(listingId: string): Promise<Listing> {
     return this.get<Listing>(`listings/${listingId}`);
+  }
+
+  getAmenities(listingId: string): Promise<Amenity[]> {
+    console.log("Making a follow-up call for amenities with ", listingId);
+    return this.get<Amenity[]>(`listings/${listingId}/amenities`);
+  }
+
+  createListing(listing: CreateListingInput): Promise<Listing> {
+    return this.post<Listing>("listings", {
+      body: {
+        listing
+      }
+    });
   }
 }
